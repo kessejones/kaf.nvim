5783f6188048c1c303b7708bb4e3e72ba1f660da
Kesse Jones <kjonesfc@outlook.com>
2024-07-08 00:10:13 -0300

    testing

--- lua/kaf/integrations/telescope/init.lua
+++ lua/kaf/integrations/telescope/init.lua
@@ -1,5 +1,5 @@
 return {
-    clients = require("kaf.integrations.telescope.clients"),
-    topics = require("kaf.integrations.telescope.topics"),
-    messages = require("kaf.integrations.telescope.messages"),
+    clients = vim.schedule_wrap(require("kaf.integrations.telescope.clients")),
+    topics = vim.schedule_wrap(require("kaf.integrations.telescope.topics")),
+    messages = vim.schedule_wrap(require("kaf.integrations.telescope.messages")),
 }
